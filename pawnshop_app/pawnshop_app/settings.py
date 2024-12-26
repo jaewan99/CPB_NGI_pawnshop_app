4b6bde52405a53a551693d37a10a9a78480a8b0d
Jaewan Bae <68039639+jaewan99@users.noreply.github.com>
2024-12-26 23:31:33 +0800

    Update settings.py

--- pawnshop_app/pawnshop_app/settings.py
+++ pawnshop_app/pawnshop_app/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret
-SECRET_KEY ='django-insecure-_9mz2%8b4ebsg_thvl2)in@=-+d#k&77_1p@qt@u7-@q89x0xy'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
